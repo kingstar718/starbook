8ac85e72b6fc343fcccd853f21abd93b41b9a89d
kingstar718 <kingstar718@foxmail.com>
2019-05-14 22:55:38 +0800

    change

---
+++ src/main/java/top/wujinxing/starbook/controller/IndexController.java
@@ -0,0 +1,14 @@
+package top.wujinxing.starbook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class IndexController {
+
+    @RequestMapping("index")
+    public String toUserList(){
+        return "index";
+    }
+}
