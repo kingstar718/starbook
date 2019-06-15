fde50e198f87814113939c4d4a3ceb135eeaf4c9
kingstar718 <kingstar718@foxmail.com>
2019-06-15 23:59:15 +0800

    user book review

--- src/main/java/top/wujinxing/starbook/service/impl/UserBookReviewServiceImpl.java
+++ src/main/java/top/wujinxing/starbook/service/impl/UserBookReviewServiceImpl.java
@@ -1,29 +1,36 @@
 package top.wujinxing.starbook.service.impl;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import top.wujinxing.starbook.dao.UserBookReviewDao;
 import top.wujinxing.starbook.entity.UserBookReview;
 import top.wujinxing.starbook.service.UserBookReviewService;
 
 import java.util.List;
 
 public class UserBookReviewServiceImpl implements UserBookReviewService {
+
+    @Autowired
+    private UserBookReviewDao userBookReviewDao;
+
+
     @Override
     public List<UserBookReview> findAll() {
+        return userBookReviewDao.findAll();
+    }
+
+    @Override
+    public UserBookReview findById(Integer reviewId) {
         return null;
     }
 
     @Override
-    public UserBookReview findById() {
+    public UserBookReview addBookReview(UserBookReview userBookReview) {
         return null;
     }
 
     @Override
-    public UserBookReview addBookReview() {
-        return null;
-    }
-
-    @Override
-    public UserBookReview updateBookReview() {
+    public UserBookReview updateBookReview(UserBookReview userBookReview) {
         return null;
     }
 
