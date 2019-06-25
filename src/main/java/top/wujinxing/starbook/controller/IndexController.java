f45860974ca76ce028f56ef5ae23d3bf30a3de38
kingstar718 <936016045@qq.com>
2019-06-25 15:19:38 +0800

    delete changes

--- src/main/java/top/wujinxing/starbook/controller/IndexController.java
+++ src/main/java/top/wujinxing/starbook/controller/IndexController.java
@@ -11,9 +11,4 @@ public class IndexController {
     public String toUserList(){
         return "index";
     }
-
-    @RequestMapping("/toMapService")
-    public String toMapService(){
-        return "map/mapService";
-    }
 }
